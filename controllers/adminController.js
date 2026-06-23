c6fd0134e6b1ff13da6382365d01a943fe61c7c2
chandanadabbi <dabbichandana599@gmail.com>
2026-06-23 11:49:10 +0530

    product routes  added

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -4,23 +4,15 @@ const Order = require("../models/orderModel");
 
 const getDashboardStats = async (req, res) => {
   try {
-    const totalProducts =
-      await Product.countDocuments();
+    const totalProducts = await Product.countDocuments();
 
-    const totalUsers =
-      await User.countDocuments();
+    const totalUsers = await User.countDocuments();
 
-    const totalOrders =
-      await Order.countDocuments();
+    const totalOrders = await Order.countDocuments();
 
-    const orders =
-      await Order.find();
+    const orders = await Order.find();
 
-    const revenue = orders.reduce(
-      (acc, order) =>
-        acc + order.totalPrice,
-      0
-    );
+    const revenue = orders.reduce((acc, order) => acc + order.totalPrice, 0);
 
     res.status(200).json({
       success: true,
@@ -37,6 +29,76 @@ const getDashboardStats = async (req, res) => {
   }
 };
 
+const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+
+    res.status(200).json({
+      success: true,
+      products,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+const createProduct = async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+
+    res.status(201).json({
+      success: true,
+      product,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+const updateProduct = async (req, res) => {
+  try {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+
+    res.status(200).json({
+      success: true,
+      product,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+const deleteProduct = async (req, res) => {
+  try {
+    await Product.findByIdAndDelete(req.params.id);
+
+    res.status(200).json({
+      success: true,
+      message: "Product Deleted",
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
 module.exports = {
   getDashboardStats,
+  getAllProducts,
+  createProduct,
+  updateProduct,
+  deleteProduct,
 };
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -6,8 +6,21 @@ const protect = require("../middleware/authMiddleware");
 
 const admin = require("../middleware/adminMiddleware");
 
-const { getDashboardStats } = require("../controllers/adminController");
+const {
+  getDashboardStats,
+  getAllProducts,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} = require("../controllers/adminController");
 
 router.get("/dashboard", protect, admin, getDashboardStats);
+router.get("/products", protect, admin, getAllProducts);
+
+router.post("/products", protect, admin, createProduct);
+
+router.put("/products/:id", protect, admin, updateProduct);
+
+router.delete("/products/:id", protect, admin, deleteProduct);
 
 module.exports = router;
